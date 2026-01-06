7ce6b75ecb0bb08fa6be688a210b3f1ec8f2b115
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-06 09:13:02 -0300

    declaração de variáveis

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+var fruta1 = 'maça'
+let fruta2 = 'morango'
+const fruta3 = 'pêssego'
+
+console.log(fruta1, fruta2, fruta3)
+
+fruta1 = 'abacate'
+fruta2 = 'melancia'
+fruta3 = 'banana' // Não é possível alterar, pois foi declarada como const
+
+console.log(fruta1, fruta2, fruta3)
+

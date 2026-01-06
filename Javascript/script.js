c4fdc17af4779c13f8a98e66d33197ef769f9669
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-06 13:30:59 -0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,4 @@
+/*
 var fruta1 = 'maça'
 let fruta2 = 'morango'
 const fruta3 = 'pêssego'
@@ -9,4 +10,22 @@ fruta2 = 'melancia'
 fruta3 = 'banana' // Não é possível alterar, pois foi declarada como const
 
 console.log(fruta1, fruta2, fruta3)
+*/
+
+//Concatenação
+
+/*
+const nome = 'Bruna'
+const idade = '27'
+const cidade = 'Schroeder - SC'
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`)
+*/
+
+const nome = 'Bruna Lais'
+const sobrenome = 'Lindner'
+const nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(`Meu nome completo é ${nomeCompleto}`)
+
 

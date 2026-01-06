7c0ede8e304a805847500cc48c222a1a2fe3931f
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-06 14:05:25 -0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -20,12 +20,45 @@ const idade = '27'
 const cidade = 'Schroeder - SC'
 
 console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`)
-*/
+
 
 const nome = 'Bruna Lais'
 const sobrenome = 'Lindner'
 const nomeCompleto = `${nome} ${sobrenome}`
 
 console.log(`Meu nome completo é ${nomeCompleto}`)
+*/
+
+// Tipos primitivos
+
+/*
+let1 = null
+let2 = 
+let3 = 10
+
+console.log(typeof let1)
+console.log(typeof let2)
+console.log(typeof let3)
+*/
+
+
+//Questão 1 : Declare uma variável do tipo number com um valor inteiro e imprima no console
+
+const num = 30
+console.log(num)
+
+//Questão 2 : Declare uma variável chamada estaChovendo e atribua a ela o valor false. Em seguida, exiba o tipo de dado de estaChovendo no console
+
+const estaChovendo = false
+
+console.log(typeof estaChovendo)
+
+//Questão 3 : Declare uma variável chamada nomeCompleto e atribua a ela um nome completo, incluindo o primeiro e o último nome. Em seguida, exiba nome completo no console
+
+const nomeCompleto = 'Bruna Lais Lindner'
+
+console.log(`O nome completo é ${nomeCompleto}`)
+
+
 
 

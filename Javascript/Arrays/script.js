4722a79149992b8feaa150058704c9dff90001b0
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 09:28:08 -0300

    arrays

---
+++ Javascript/Arrays/script.js
@@ -0,0 +1,5 @@
+// Crie um array chamado Cores, que contenha pelo menos cinco cores e escreva um código que imprima a terceira cor
+
+const cores = ['Vermelho' , 'Amarelo' , 'Azul', 'Rosa', 'Verde']
+
+console.log(cores[2]);

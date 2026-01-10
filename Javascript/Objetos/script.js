834532c9a9ee8f45061eb815ef5cc3d7bef4b908
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 10:05:04 -0300

    objetos

---
+++ Javascript/Objetos/script.js
@@ -0,0 +1,18 @@
+/*const carro = {
+    marca: 'Toyota',
+    modelo : 'Corolla',
+    ano: 2025
+}
+
+console.log(carro)
+*/
+
+/*
+const carro = {}
+carro.marca = 'Toyota'
+carro.modelo = 'Corolla'
+carro.ano = 2025
+
+console.log(carro)
+*/
+

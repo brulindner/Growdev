4a790dda44f4ed71835d35c541a3f252e9e50983
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 15:21:06 -0300

    operadores de atribuição

---
+++ Javascript/Operadores de Atribuição/script.js
@@ -0,0 +1,15 @@
+// Questão 01: Um cliente fez um depósito de R$ 200,00 em sua conta bancária, que tinha um saldo de R$ 1.000,00. Atualize o saldo da conta 
+
+let saldoConta = 1000
+const deposito = 200
+
+saldoConta += deposito
+console.log(saldoConta)
+
+// Questão 02: Uma loja vendeu 2 unidades de um produto que possui 50 unidades em estoque. Atualize o estoque
+
+let estoque = 50
+const quantidadeVenda = 3
+
+estoque -= quantidadeVenda
+console.log(estoque)

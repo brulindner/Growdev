3af986919531b3a351ef4113e9563bba5c0a56f3
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 17:01:36 -0300

    precedencia de operadores

---
+++ Javascript/Precedência de Operadores/script.js
@@ -0,0 +1,18 @@
+//Questão 01: Você está desenvolvendo um sistema para calcular o preço final de um produto após aplicar um desconto. O produto custa 100 reais e tem um desconto de 20%. Em seguida, você precisa calcular o preço final após adicionar 5 reais de taxa de entrega 
+
+custoProduto = 100
+desconto = 0.80
+taxaEntrega = 5
+
+precoFinal = (custoProduto * desconto) + taxaEntrega
+console.log(precoFinal)
+
+//Questão 02: Você está ajudando a calcular a média das notas de uma pessoa estudante. As notas são armazenadas em variáveis e a média deve ser calculada considerando as notas de três provas. Defina as variáveis de nota e calcule a média
+
+const nota1 = 8.5
+const nota2 = 9
+const nota3 = 10
+
+const media = (nota1 + nota2 + nota3) / 3
+
+console.log(media)

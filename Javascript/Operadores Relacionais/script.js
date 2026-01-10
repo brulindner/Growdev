782cc4b8af650252ee688eb792caf1ec43831bc7
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 15:41:34 -0300

    operadores relacionais

---
+++ Javascript/Operadores Relacionais/script.js
@@ -0,0 +1,13 @@
+//Questão 01: Um cliente tem 17 anos e o produto que ele quer comprar, exige idade mínima de 18 anos. Verifique se ele pode comprar o produto
+
+const idadeCliente = 17
+const idadeMinima = 18
+
+console.log(idadeCliente >= idadeMinima)
+
+//Questão 02: Um cliente deseja realizar uma compra de R$ 300,00 mas seu limite de crédito é de R$ 500,00. Verifique se a compra está dentro do limite de crédito
+
+const valorCompra = 300
+const limite = 500
+
+console.log(valorCompra <= limite)

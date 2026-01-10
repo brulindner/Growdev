8b15eeec06c034505fc7d0fe214828c7b3a270ef
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 15:03:23 -0300

    operadores aritmeticos

---
+++ Javascript/Operadores Aritmeticos/script.js
@@ -0,0 +1,16 @@
+//Questão 01: Dado que um funcionário trabalha 8 horas por dia e trabalhou 5 dias na semana, calcule o total de horas trabalhadas:
+
+const horasTrabalhadas = 8
+const diasTrabalhados = 5
+
+const totalHoras = horasTrabalhadas * diasTrabalhados
+console.log(totalHoras)
+
+//Questão 02: O preço de um produto é R$50,00 e um cliente comprou 3 unidades. Ele recebeu um desconto de R$10,00 no valor final. Calcule o valor total da compra após aplicar o desconto
+
+const precoProduto = 50.00
+const quantidadeComprada = 3
+const desconto = 10.00
+
+const totalCompra = (precoProduto * quantidadeComprada) - desconto
+console.log(totalCompra)

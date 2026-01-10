f153a46b2165323d0b42d7675e085809b49ced5d
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-10 14:26:52 -0300

    Update script.js

--- Javascript/Objetos/script.js
+++ Javascript/Objetos/script.js
@@ -16,3 +16,16 @@ carro.ano = 2025
 console.log(carro)
 */
 
+// Crie um objeto que representa um aluno. Com as propriedades nome, idade, nota e curso. Exiba cada informação desse aluno no console. 
+
+const aluno = {
+nome : 'Lucas',
+idade : 27,
+nota : 9.8,
+curso : 'Engenharia de Software'
+}
+
+console.log('Nome do aluno:' , aluno.nome)
+console.log('Idade do aluno:' , aluno.idade)
+console.log('Nota e do aluno:' , aluno.nota)
+console.log('Curso do aluno:' , aluno.curso)

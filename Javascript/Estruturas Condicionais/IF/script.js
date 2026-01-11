48cafd50de81ca89ddd0be3206534d63deb2ed9b
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-11 15:19:14 -0300

    Estruturas condicionais - IF

---
+++ Javascript/Estruturas Condicionais/IF/script.js
@@ -0,0 +1,37 @@
+
+/*
+//Questão 1: Crie um programa que recebe um número e verifica se ele é par. Caso seja, exiba "O número é par"
+
+
+let number = Number(prompt("Informe um número para verificar se é par ou ímpar"))
+
+if(number %2 == 0){
+    console.log(`O número ${number} é par!` )
+} else{console.log(`O número  ${number} é ímpar!`)}
+*/
+
+
+
+/*
+// Questão 2: crie um programa que recebe um número e verifica se ele é negativo. Caso seja, exiba "O número é negativo"
+
+let num = Number(prompt("Informe um número para verificar se é negativo"))
+
+if(num < 0){
+    console.log(`O número ${num} é negativo`)
+} else{
+    console.log(`O número ${num} é positivo`)
+}
+*/
+
+
+
+//Questão 3: Crie um programa que recebe um valor e verifica se ele é maior que um limite de R$ 100,00. Se for, exiba "O valor excede o limite"
+
+let valor = parseFloat(prompt("Informe o valor do depósito: "))
+
+if(valor > 100){
+    console.log("O valor excede o limite")
+}
+
+

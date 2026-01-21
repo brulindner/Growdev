c9fb6a550ae0fa45f99e6e92253af35d89152568
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-21 20:19:23 -0300

    Update script.js

--- Javascript/Estruturas Condicionais/Switch/script.js
+++ Javascript/Estruturas Condicionais/Switch/script.js
@@ -1,3 +1,4 @@
+/*
 //Crie uma estrutura switch que recebe uma variável nota (A, B, C, D ou F) e exiba uma mensagem correspondente a avaliação:
 
 //A: "Excelente!"
@@ -31,4 +32,50 @@ switch (nota){
 }
 
 console.log(mensagem) 
+*/
 
+//IF ELSE ou Switch Case? 
+
+//Questão 01: Crie um programa que recebe a hora do dia e exibe se é "manhã", "tarde", "noite" ou "hora inválida". Considere:
+
+// - Manha 05h as 11h
+// - Tarde: 12h as 17h
+// - Noite: 18h as 22h
+
+/*
+const hora = 13
+
+if(hora >=5 && hora <=11){
+    console.log("Manhã")
+}
+else if(hora >=12 && hora <=17){
+ console.log("Tarde")
+}
+else if(hora >=18 && hora<=22){
+    console.log("Noite")
+}
+else{
+    console.log("Hora inválida")
+}
+*/
+
+//Questão 02:
+
+const cor = 'vermelho'
+
+switch(cor){
+    case 'vermelho':
+        console.log("Você escolheu a cor da paixão")
+        break
+
+    case 'azul':
+        console.log("Você escolheu a cor da tranquilidade")
+        break
+
+    case 'verde':
+        console.log("Você escolheu a cor da natureza")
+        break
+
+    default:
+        console.log("Cor desconhecida")
+}

7ba5406ecabbb85307fdd4124ff786d27bcff3d0
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-22 21:45:45 -0300

    Update script.js

--- Javascript/Estruturas de Repetição/script.js
+++ Javascript/Estruturas de Repetição/script.js
@@ -1,5 +1,6 @@
 // WHILE
 
+/*
 const numeroAleatorio = Math.floor(Math.random()*10)+1
 let tentativas = 0
 let acertou = false
@@ -17,3 +18,24 @@ while(!acertou){
     }
 
 }
+    */
+
+/*
+//Crie um programa que faça contagem regressiva de um número até 0. O programa deve imprimir cada número da contagem no console. 
+
+numero = parseInt(prompt('Digite um número: '))
+
+if(numero > 0){
+    while(numero >= 0){
+        console.log(numero)
+        numero --
+    
+        if(numero == 0){
+            console.log('Contagem finalizada')
+        }
+    } else{
+        console.log('Por favor, digite um número inteiro positivo')
+    }
+}
+    */
+

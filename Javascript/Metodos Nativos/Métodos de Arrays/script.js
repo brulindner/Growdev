3c08838f629619e103e3123a578cc38f9ab1420a
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-27 20:08:20 -0300

    Update script.js

--- Javascript/Metodos Nativos/Métodos de Arrays/script.js
+++ Javascript/Metodos Nativos/Métodos de Arrays/script.js
@@ -90,3 +90,50 @@ console.table(recadosUrgentes)
 let recadosJavascript = recados.filter((recado) => recado.descricao.includes("Javascript"));
 
 console.table(recadosJavascript)
+
+//EXERCICIOS
+
+
+// 1 - Um usuário tem uma lista de números e deseja filtrar apenas os números pares
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let numerosPares = numeros.filter((numero)=> numero%2 ===0)
+console.log(numerosPares)
+    
+
+// 2 - Uma cliente tem uma lista de nomes e deseja filtrar apenas os nomes que iniciam com a palavra "João"
+let nomes = ["João", "Maria", "Daphne", "Pedro", "João Pedro", "Ana", "José"];
+
+let nomeJoao = nomes.filter((nomes) => nomes.startsWith("João"))
+
+console.table(nomeJoao)
+
+//3 - Um usuário tem uma lista de objetos onde cada objeto possui várias propriedades, incluindo uma informação sobre o nível de urgencia (de 1 a 5), sendo 1 pouco urgente e 5 muito urgente. O objetivo é filtrar apenas os objetos onde o campo urgente é maior ou igual a 3
+
+let itens = [
+    {nome: "Item 1", categoria: "Eletrônicos", urgente:5},
+    {nome: "Item 2", categoria: "Livros", urgente:3},
+    {nome: "Item 3", categoria: "Alimentos", urgente:4},
+    {nome: "Item 4", categoria: "Roupas", urgente:2},
+    {nome: "Item 5", categoria: "Ferramentas", urgente:5},
+]
+
+let objetoUrgente = itens.filter((itens => itens.urgente>=3))
+
+console.table(objetoUrgente)
+
+//4 - Um cliente tem uma lista de cidades com seus respectivos estados e deseja filtrar apenas as cidades que estão localizadas no estado do Rio Grande do Sul
+
+let cidades = [
+    {nome: "Porto Alegre", estado: "RS"},
+    {nome: "Pelotas", estado: "RS"},
+    {nome: "Caxias do Sul", estado: "RS"},
+    {nome: "Curitiba", estado: "PR"},
+    {nome: "Florianópolis", estado: "SC"},
+]
+
+let cidadesRs = cidades.filter((cidades)=> cidades.estado=="RS")
+
+console.table(cidadesRs)
+   
+ 

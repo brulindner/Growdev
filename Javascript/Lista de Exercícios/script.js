77cc5b44efcc30dd6788538e828dad80e284d587
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-01-31 22:49:38 -0300

    exercicios

---
+++ Javascript/Lista de Exercícios/script.js
@@ -0,0 +1,10 @@
+/*
+// Ex 01 - Idade do usuário
+
+let idade = prompt("Qual é a sua idade?")
+alert(`Minha idade é ${idade} anos`)
+console.log(`Minha idade é ${idade} anos`)
+*/
+
+//Ex 02 - Soma de dois números
+
